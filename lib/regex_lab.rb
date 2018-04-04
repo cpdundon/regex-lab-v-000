a0f668909cfe1933443b5bfa3fe840a6b6cffd0a
Chris Dundon <chris.dundon@gmail.com>
2018-04-04 18:22:34 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -36,8 +36,6 @@ def first_word_capitalized_and_ends_with_punctuation?(text)
 end
 
 def valid_phone_number?(phone)
-  puts phone
   ret = phone.match(/^.*\d{3}.*\d{3}.*\d{4}$/)
-  puts ret
   !!ret
 end
