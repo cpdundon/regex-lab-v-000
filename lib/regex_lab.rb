5015e38940b6e7b739b1d3619a87d09473eadc04
Chris Dundon <chris.dundon@gmail.com>
2018-04-04 18:01:37 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -31,7 +31,7 @@ def words_five_letters_long(text)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-  text.scan(/[A-Z][a-z]/)
+  text.scan(/[A-Z][a-z]*/)
 end
 
 def valid_phone_number?(phone)
